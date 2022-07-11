e5eca77d79bd1ffcfe2b0ceb041a2b6ebcdb23be
Guok Wei Jie <guokweijie@gmail.com>
2022-07-11 09:40:45 +0800

    group order page layout

--- src/screens/TestingScreen.js
+++ src/screens/TestingScreen.js
@@ -12,6 +12,7 @@ import {
     Keyboard,
 } from 'react-native';
 import React, { useState, useEffect } from 'react';
+import { AuthTextInput, AuthPressable } from '../components';
 import { query, collection, onSnapshot, addDoc, deleteDoc, doc } from 'firebase/firestore';
 
 import { db } from '../firebase';
@@ -19,10 +20,57 @@ import { Task } from '../components';
 
 function TestingScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Testing Screen</Text>
-    </View>
-  );
+    <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={Platform.OS === 'ios' ? 'padding' : null}
+            >
+                <View style={styles.container}>
+                    <Text style={[styles.welcomeText, styles.boldText]}>
+                        {`Start Your Order Now!`}
+                    </Text>
+                    <AuthTextInput
+
+                        placeholder="Grab Vendor"
+
+                        keyboardType="email-address"
+                    />
+                    <AuthTextInput
+
+                        placeholder="Grab Group Order Link"
+                    />
+                     <AuthTextInput
+
+                        placeholder="Service Fee on Grab"
+
+                     />
+                     <AuthPressable
+                        //onPressHandler={isLogin ? loginHandler : signUpHandler}
+                        title={'Send Notifications!'}
+                     />
+                </View>
+        </KeyboardAvoidingView>
+    );
 }
 
 export default TestingScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#EBECF0',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    boldText: {
+        fontWeight: '400',
+    },
+    welcomeText: {
+        fontSize: 32,
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    authText: {
+        fontSize: 20,
+        marginBottom: 10,
+    },
+});
